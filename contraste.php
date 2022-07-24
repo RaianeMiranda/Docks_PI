38c8f9e690691b48222747b3973c8ce30cfb5504
Raiane Miranda <raiane.oliveira@alunos.sc.senac.br>
2022-07-24 14:25:39 -0300

    Adicionando o código de contraste

---
+++ contraste.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Cor fundo</title>
+<link rel="alternate stylesheet" href="assets/css/escuro.css" title="2">
+<link rel="alternate stylesheet" href="assets/css/claro.css" title="1">
+<link rel="stylesheet" href="assets/css/b_contraste.css">
+<script src="https://code.jquery.com/jquery-1.11.3.js"></script>
+<script type="text/javascript" src="assets/js/altocontraste.js"></script>
+</head>
+<body>
+<button id="click" class="contraste">Contraste</button>
+<h2 alt="Bem vindo escritor/escritora">Bem vindo escritor(a)  </h2>
+</body>
+</html>
