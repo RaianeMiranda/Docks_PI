7d132a2d952367e4efa0c651f3b99a047abf96d5
Raiane Miranda <raiane.oliveira@alunos.sc.senac.br>
2022-07-24 16:04:19 -0300

    Criando página de login, alterações iniciais

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="login-form">
+        <div class="todoLogin">
+            <div class="login_cima">
+                <img class="logo" src="assets/images/logo.png" alt="logo do site, sendo um círculo com nosso mascote docks, um pato dentro desse círculo.">
+                <h1 class="slogan">Bem vindo(a) ao Docks</h1>
+                <p class="slogan">Frase de slogan!!</p>
+            </div>
+
+            <div class="login-meio">
+            <fieldset>
+                <legend>E-mail</legend>
+                <input class="" type="text" placeholder="Digite seu E-mail">
+            </fieldset>
+            </div>
+         <!-- 
+            <div class="login-meio">
+            <label for="e-mail">E-mail</label>
+            <input class="" type="text" name="email" id="e-mail" placeholder="Digite seu E-mail">
+            </div>
+        -->
+
+    
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
