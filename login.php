b241214e69413f466e9bb6ebf4ecdb2216d56050
Raiane Miranda <raiane.oliveira@alunos.sc.senac.br>
2022-07-25 18:44:49 -0300

    Criando login, metade pronto

--- login.php
+++ login.php
@@ -1,9 +1,14 @@
+<?php
+include "libras.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="assets/css/login.css">
+    <script src="https://kit.fontawesome.com/9e39b5f510.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 
@@ -11,26 +16,63 @@
     <div class="login-form">
         <div class="todoLogin">
             <div class="login_cima">
-                <img class="logo" src="assets/images/logo.png" alt="logo do site, sendo um círculo com nosso mascote docks, um pato dentro desse círculo.">
+                <div class="flex2">
+                    <img class="logo" src="assets/images/logo.png" alt="logo do site, sendo um círculo com nosso mascote docks, um pato dentro desse círculo.">
+                    <button class="exit-icon"><i class="fa-solid fa-xmark"></i></button>
+                    <!--CONTINUAR DAQUI, CRIAR GRID PARA SEPARAR ÍCONE DA LOGO-->
+                </div>
+
                 <h1 class="slogan">Bem vindo(a) ao Docks</h1>
-                <p class="slogan">Frase de slogan!!</p>
+                <p class="slogan">Aqui é o lugar para suas histórias</p>
             </div>
 
             <div class="login-meio">
-            <fieldset>
-                <legend>E-mail</legend>
-                <input class="" type="text" placeholder="Digite seu E-mail">
-            </fieldset>
+                <fieldset>
+                    <legend>Digite seu E-mail</legend>
+                    <input class="norm-login" type="text" placeholder="E-mail">
+                </fieldset>
+
+                <fieldset>
+                    <legend>Digite sua Senha</legend>
+                    <input class="norm-login" type="password" placeholder="Senha">
+                </fieldset>
+
+                <button class="continuar"> Continuar </button>
+
+                <p class="ou">OU</p>
             </div>
-         <!-- 
+
+            <div class="login-baixo">
+
+                <fieldset>
+                    <div class="flex">
+                        <img class="icon-login" src="assets/images/facebook.png" alt="Faça seu login com o Facebook">
+                        <button class="login-with">Continue com o Facebook </button>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <div class="flex">
+
+                        <img class="icon-login" src="assets/images/google.png" alt="Faça seu login com o Facebook">
+                        <button class="login-with">Continue com o Google </button>
+                    </div>
+                </fieldset>
+
+
+            </div>
+        </div>
+
+    </div>
+    <!-- 
             <div class="login-meio">
             <label for="e-mail">E-mail</label>
             <input class="" type="text" name="email" id="e-mail" placeholder="Digite seu E-mail">
             </div>
         -->
 
-    
-        </div>
+
+    </div>
     </div>
     </div>
 </body>
