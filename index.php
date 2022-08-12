995a6a793465254298bd076005eafbf0d417cb9c
Clara <clara.viana@alunos.sc.senac.br>
2022-08-12 14:56:52 -0300

    adicionando informações sobre o site

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sobre nós</title>
+</head>
+
+<body>
+    <h1>Sobre nós</h1>
+    <p>O Docks é uma plataforma de aprendizagem, que foi criado por
+        leitoras com o objetivo de suprir a carência de aplicativos na área da escrita.
+    </p>
+    <p>O site proporciona a aprendizagem do método de escrita , o Snowflake e outros recursos como: Criação de Mundos, Personagens e o roteiro da Jornada do Herói.
+        Além das atividades diárias para o aumento da criatividade.</p>
+    <p>O objetivo final é a criação de um livro bem estruturado, completo e sem furos.
+    </p>
+    <p>O objetivo final é a criação de um livro bem estruturado, completo e sem furos.
+    </p>
+    <p>Os escritores iniciantes são a nossa principal motivação, já que são
+    </p>
+    <p>A nossa equipe visa com o Docks tornar fácil a criação de um livro e aprendizado acessível para e
+        <h1>Nossa História</h1>
+        <p>Dock</p>
+        <p>Idade:</p>
+        <p>Ocupação:</p>
+        <p>Signo:</p>
+        <p>Idade:</p>
+        <p>Backstory:</p>
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,23 @@
+<nav>
+    <div class="item">
+        <div class="logo-nome">
+            <div class="menu-container">
+                <a href="#" class="container-menu-burger">
+                    <i class="menu-burger">
+                    </i>
+                </a>
+            </div>
+            <img src="assets/images/docks.png" class="logotipo">
+            <h1 class="nome">Docks</h1>
+        </div>
+    </div>
+    <ul>
+
+        <li><b><a href="personagens.php">Criação de Personagens</a></b></li>
+        <li><b><a href="biblioteca.php">Biblioteca</a></b></li>
+        <li><b><a href="jornada.php">Jornada do Herói</a></b></li>
+        <li><b><a href="snowflake.php">Snowflake</a></b></li>
+        <li><b><a href="mundos.php">Criação de Mundos</a></b></li>
+    </ul>
+    <hr class="linha">
+</nav>
