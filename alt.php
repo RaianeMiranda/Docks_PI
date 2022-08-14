3dc1bd3160a0d3534688d782d1d61405b4eb30b4
HeloisaRebello17 <heloisarebellocabral.79@gmail.com>
2022-08-14 19:00:16 -0300

    adicionando alterações da conta/sem background

---
+++ alt.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alterações da conta</title>
+    <?php
+    $titulo= "Alterações da conta";
+    ?>
+    <link rel="stylesheet" href="assets/css/alt.css">
+</head>
+
+<body>
+    <div class="alt-container">
+        <div class="alt-titulo">
+            <h1>Alterações da conta</h1>
+        </div>
+        <p class="alt-1"><b>Altere seu Email</b></p><input type="text" placeholder=" Seu E-mail Atual">
+        <br>
+        <input type="text" placeholder=" Seu novo E-mail">
+        <br>
+        <p class="alt-2"><b>Altere sua senha</b></p><input type="password" placeholder=" Sua senha atual">
+        <br>
+        <input type="password" placeholder=" Sua nova senha">
+        <br>
+        <form>
+            <button>Salvar</button>
+        </form>
+    </div>
+
+
+</body>
+
+</html>
