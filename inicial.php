8b26a863efb278e635a96170253caa7c3a77f47a
Raiane Miranda <raiane.oliveira@alunos.sc.senac.br>
2022-08-17 13:47:59 -0300

    Criando página inicial, testando com cards e carousel

--- inicial.php
+++ inicial.php
@@ -2,9 +2,12 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/inicial.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <?php include "head.php" ?>
+    </header>
 </body>
 </html>
