fdc74cf735e0a640f762f7c3542f0b46385a429e
Clara <clara.viana@alunos.sc.senac.br>
2022-08-19 17:08:34 -0300

    adicionando sobre nós

---
+++ sobrenos.php
@@ -0,0 +1,89 @@
+<!--
+
+<body>
+<div class="item">
+        <a href="#" class="container-menu-burger">
+            <i class="menu-burger">
+            </i>
+        </a>
+    </div> -->
+<!-- </main> -->
+
+</*?php include "nav.php" ; ?>
+*/-->
+
+<html>
+
+<head>
+    <title>sobre nós</title>
+    <meta name="description" content="Saiba quais as principais linguagens para desenvolvimento Web: HTML, JavaScript e PHP">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+</head>
+
+<body>
+    <h1>Sobre nós</h1>
+    <p>O Docks é uma plataforma de aprendizagem, que foi criado por
+        leitoras com o objetivo de suprir a carência de aplicativos na área da escrita.
+    </p>
+    <p>O site proporciona a aprendizagem do método de escrita , o Snowflake e outros recursos como: Criação de Mundos, Personagens e o roteiro da Jornada do Herói.
+        Além das atividades diárias para o aumento da criatividade.</p>
+    <p>O objetivo final é a criação de um livro bem estruturado, completo e sem furos.
+        Os escritores iniciantes são a nossa principal motivação, já que são
+        A nossa equipe visa com o Docks tornar fácil a criação de um livro e aprendizado acessível
+        Os escritores iniciantes são a nossa principal motivação, já que são pessoas independentes e sem auxílio algum, que através do Docks desenvolvem melhor a escrita e estrutura de seus livros, possibilitando assim o crescimento de escritores na literatura nacional.</p>
+    A nossa equipe visa com o Docks tornar fácil a criação de um livro e aprendizado acessível para escritores .</p>
+    <br>
+    <h1>Nossa história</h1>
+    <div class="container2">
+        <img src="assets/images/dock.png">
+        <div class="container">
+            <h1>Dock</h1>
+            <p>Idade:</p>
+            <p>Ocupação:</p>
+            <p>Signo:</p>
+            <p>Idade:</p>
+            <p>Backstory:</p>
+        </div>
+    </div>
+    <div class="container2">
+        <p><img src="assets/images/pate.png">
+            <div class="container">
+                <h1>Paty</h1>
+                <p>Idade:</p>
+                <p>Ocupação:</p>
+                <p>Signo:</p>
+                <p>Idade:</p>
+                <p>Backstory:</p>
+                <br>
+            </div>
+    </div>
+
+    <div class="container2">
+        <p><img src="assets/images/mentor.png">
+            <div class="container">
+                <h1>Mentor</h1>
+                <p>Idade:</p>
+                <p>Ocupação:</p>
+                <p>Signo:</p>
+                <p>Idade:</p>
+                <p>Backstory:</p>
+                <br>
+            </div>
+    </div>
+    <div class="container2">
+        <p><img src="assets/images/psicopato.png">
+            <div class="container">
+                <h1>Psicopato</h1>
+                <p>Idade:</p>
+                <p>Ocupação:</p>
+                <p>Signo:</p>
+                <p>Idade:</p>
+                <p>Backstory:</p>
+            </div>
+    </div>
+
+
+</body>
+
+</html>
