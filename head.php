007b7f8cde12a67c6d05f48d95d036929bdb8eb1
Raiane Miranda <raiane.oliveira@alunos.sc.senac.br>
2022-08-23 15:23:53 -0300

    adicionando head perdido na merge sem o nav

---
+++ head.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        <?php
+        echo $titulo;
+        ?>
+    </title>
+    <link rel="stylesheet" href="assets/css/nav.css">
+    <link rel="stylesheet" href="assets/css/burger.css">
+
+</head>
+
+<body>
+    <!-- <div class="item">
+        <a href="#" class="container-menu-burger">
+            <i class="menu-burger">
+            </i>
+        </a>
+    </div> -->
+    <!-- </main> -->
+
+    <?php
+    include "nav.php";
+    ?>
+
+</body>
+
+</html>
