73f7b460b8426dc73d887a2b9013906eb1a968ef
helenameirelles21 <101889817+helenameirelles21@users.noreply.github.com>
2022-08-30 15:04:42 -0300

    Create modal.php
    "criação do arquivo modal em php"

---
+++ modal.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="assets/css/helena.css">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Alterações</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="tipografia">
+                    <p class="negrito-titulo"><b>Negrito</b></p>
+                            <p class="italico-titulo"><i>Itálico</i></p>
+                </div>
+                <div class="foto-tipografia">
+                    <button type="button" class="negrito"><img  src="assets/images/negrito.png"></button>
+                    <button type="button" class="italico"><img  src="assets/images/italico.png"></button>
+                </div>
+                <div class="marcadores">
+                    <div class="c1">1</div>
+                    <div class="c2">1</div>
+                     <div class="c3">1</div>
+                     <div class="c4">1</div>
+                      <div class="c5">1</div>
+                      <button class="c6">Nenhuma</button>
+                   
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
