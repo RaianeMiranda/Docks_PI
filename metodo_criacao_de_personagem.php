6d737ddf3203f21b6d847f26a5d2e7c6e07d9de4
Heloisa <heloisa.cabral@alunos.sc.senac.br>
2022-09-06 16:19:47 -0300

    ajustando o site

--- metodo_criacao_de_personagem.php
+++ metodo_criacao_de_personagem.php
@@ -1,3 +1,7 @@
+<?php
+$titulo = "Método Criação de Personagem";
+include "head.php"
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,32 +17,32 @@
 </head>
 
 <body>
-    <h1 class="metodo-cp-titulo">Criação de Personagem </h1>
-    <div class="descricao-container">
-    <p>Uma boa narrativa quase sempre é movida por personagens marcantes. Às vezes, marcantes em suas grandiosas personalidades e habilidades mirabolantes. Outras, ficam presas na nossa memória por sua pura simplicidade e realismo, que remete intimamente às nossas vidas.
-</p>
-       <p> O que não muda de um modelo para o outro é que uma boa personagem é uma personagem complexa, bem desenvolvida. E esse desenvolvimento é feito em camadas.</p>
-      <br>
-       <p>Na metodologia a ser apresentada, trabalharemos com três níveis de criação. </p>
-</div>
-       <ul class="camadas-container">
-        <div class="camadas-descricao-container">
-            <li class="camadas">Camada periférica: </li>
-            <p class="camadas-descricao">
-                É a parte mais fácil e rápida a ser feita para criar o seu personagem. Aqui vamos pensar em como nosso personagem é fisicamente, como ele se comunica com outras pessoas e os dados básicos.
+    <div class="container">
+        <h1 class="metodo-cp-titulo">Criação de Personagem </h1>
+        <div class="descricao-container">
+            <p>Uma boa narrativa quase sempre é movida por personagens marcantes. Às vezes, marcantes em suas grandiosas personalidades e habilidades mirabolantes. Outras, ficam presas na nossa memória por sua pura simplicidade e realismo, que remete intimamente às nossas vidas.
             </p>
+            <p class="juncao"> O que não muda de um modelo para o outro é que uma boa personagem é uma personagem complexa, bem desenvolvida. E esse desenvolvimento é feito em camadas.</p>
+        
+            <p class="separado">Na metodologia a ser apresentada, trabalharemos com três níveis de criação. </p>
         </div>
-        <div class="camadas-descricao-container">
-            <li class="camadas">A camada de entorno:</li>
-            <p class="camadas-descricao">Nesta camada, entenderemos como o personagem chegou até aqui, analisando seu histórico de vida e como foi moldada a sua personalidade. Basicamente é a biografia dele.
-            </p>
-        </div>
-        <div class="camadas-descricao-container">
-            <li class="camadas">A camada central:</li>
-            <p class="camadas-descricao"> Talvez a etapa mais desafiadora na hora de criar a ficha de personagem. Aqui desenvolvemos a alma, a essência do personagem.
-            </p>
-        </div>
-    </ul>
+        <ul class="camadas-container">
+
+            <li>
+                <span class="camadas">Camada periférica:</span> <span> É a parte mais fácil e rápida a ser feita para criar o seu personagem. Aqui vamos pensar em como nosso personagem é fisicamente, como ele se comunica com outras pessoas e os dados básicos.</span>
+            </li>
+
+            <li>
+                <span class="camadas"> A camada de entorno: </span><span>Nesta camada, entenderemos como o personagem chegou até aqui, analisando seu histórico de vida e como foi moldada a sua personalidade. Basicamente é a biografia dele.
+                </span>
+            </li>
+            <li>
+                <span class="camadas"> A camada central:</span><span> Talvez a etapa mais desafiadora na hora de criar a ficha de personagem. Aqui desenvolvemos a alma, a essência do personagem.
+                </span>
+            </li>
+
+        </ul>
+    </div>
 </body>
 
 </html>
--- metodo_snowflake.php
+++ metodo_snowflake.php
@@ -1,3 +1,7 @@
+<?php 
+ $titulo = "Método Snowflake";
+include "head.php"
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,13 +10,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Método Snowflake</title>
-    <?php
-    $titulo = "Método Snowflake"
-    ?>
     <link rel="stylesheet" href="assets/css/metodo_snowflake.css">
 </head>
 
 <body>
+<div class="container">
     <h1 class="titulo-snowflake">Snowflake</h1>
     <p>O nome deriva do inglês e significa “floco de neve”, e a ideia é visualizar o floco de neve como uma figura complexa, que é desenvolvida por formas simples que, quando colocadas uma junto à outra, evoluem até formar uma figura bem desenvolvida.
         Esse método é ótimo para você que tem uma ideia na cabeça, mas ainda não sabe como passá-la para o papel ou como organizá-la sem furos. O método pega o comecinho dessa ideia, da forma que ela surgiu e desenvolve através de alguns passos.
@@ -32,6 +34,7 @@
 <div class="etapa_4-container">
     <h2 class="etapa">Etapa 4: </h2>
     <p class="etapa-descricao">expandir o parágrafo inicial em vários parágrafos</p>
+</div>
 </div>
 </body>
 
