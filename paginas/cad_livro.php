0aa6d5e3a788bdab654bd705efe8ed8fb9ea4d5b
Raiane Miranda <raiane.oliveira@alunos.sc.senac.br>
2022-09-14 14:22:39 -0300

    corrigindo conexões do banco de dados com php e front no cad_livros.php

--- paginas/cad_livro.php
+++ paginas/cad_livro.php
@@ -1,83 +1,120 @@
 <?php
-    include "../include/MySql.php";
-    session_start();
-    $nomeLivro = "";
+include "../include/MySql.php";
+session_start();
+$nomeLivro = "";
 
-    $nomeLivroErro = "";
-    $msgErro = "";
-echo "aqui:".$_SESSION['idEmail'];
-    if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
-        if (!empty($_FILES["image"]["name"])) {
-            //Pegar informações do arquivo
-            $fileName = basename($_FILES['image']['name']);
-            $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
-            //Array de extensoes permitidas
-            $allowTypes = array('jpg', 'png', 'jpeg', 'gif');
+$nomeLivroErro = "";
+$msgErro = "";
+echo "aqui:" . $_SESSION['idEmail'];
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
+    if (!empty($_FILES["image"]["name"])) {
+        //Pegar informações do arquivo
+        $fileName = basename($_FILES['image']['name']);
+        $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
+        //Array de extensoes permitidas
+        $allowTypes = array('jpg', 'png', 'jpeg', 'gif');
 
-            if (in_array($fileType, $allowTypes)) {
-                $image = $_FILES['image']['tmp_name'];
-                $imgContent = file_get_contents($image);
+        if (in_array($fileType, $allowTypes)) {
+            $image = $_FILES['image']['tmp_name'];
+            $imgContent = file_get_contents($image);
 
-                if (empty($_POST['nome']))
-                    $nomeLivroErro = "Nome é obrigatório!";
-                else
-                    $nomeLivro = $_POST['nome'];
+            if (empty($_POST['nome']))
+                $nomeLivroErro = "Nome é obrigatório!";
+            else
+                $nomeLivro = $_POST['nome'];
 
-                if ($nomeLivro) {
-                    //Verificar se ja existe o idEmail
-                    $sql = $pdo->prepare("SELECT * FROM LIVROS WHERE nomeLivro = ?");
-                    if ($sql->execute(array($nomeLivro))) {
-                        if ($sql->rowCount() <= 0) {
-                            $sql = $pdo->prepare("INSERT INTO LIVROS (codLivro, nomeLivro, capaLivro, idEmail)
+            if ($nomeLivro) {
+                //Verificar se ja existe o idEmail
+                $sql = $pdo->prepare("SELECT * FROM LIVROS WHERE nomeLivro = ?");
+                if ($sql->execute(array($nomeLivro))) {
+                    if ($sql->rowCount() <= 0) {
+                        $sql = $pdo->prepare("INSERT INTO LIVROS (codLivro, nomeLivro, capaLivro, idEmail)
                                                 VALUES (NULL, ?, ?, ?)");
-                            if ($sql->execute(array($nomeLivro, $imgContent, $_SESSION['idEmail']))) {
-                                $msgErro = "Dados cadastrados com sucesso!";
-                                $_SESSION['nomeLivro'] =$nomeLivro;
-  
-                                $nomeLivro = "";  
-                                //header('location:../inicial.php');
-                            } else {
-                                $msgErro = "Dados não cadastrados!";
-                            }
-                        } 
-                    } else {
-                        $msgErro = "Erro no comando SELECT!";
+                        if ($sql->execute(array($nomeLivro, $imgContent, $_SESSION['idEmail']))) {
+                            $msgErro = "Dados cadastrados com sucesso!";
+                            $_SESSION['nomeLivro'] = $nomeLivro;
+
+                            $nomeLivro = "";
+                            //header('location:../inicial.php');
+                        } else {
+                            $msgErro = "Dados não cadastrados!";
+                        }
                     }
                 } else {
-                    $msgErro = "Dados não cadastrados!";
+                    $msgErro = "Erro no comando SELECT!";
                 }
             } else {
-                $msgErro = "Somente arquivos JPG, JPEG, PNG e GIFF são permitidos";
+                $msgErro = "Dados não cadastrados!";
             }
         } else {
-            $msgErro = "Imagem não selecionada!!";
+            $msgErro = "Somente arquivos JPG, JPEG, PNG e GIFF são permitidos";
         }
+    } else {
+        $msgErro = "Imagem não selecionada!!";
     }
+}
 
-    ?> <!DOCTYPE html>
-    <html>
+?>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <meta charset="UTF-8">
-        <title>Cadastro de Usuário</title>
-        <link rel="stylesheet" href="../assets/">
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Criar Livros</title>
+    <link rel="stylesheet" href="../assets/css/criar_livros.css">
+</head>
 
-    <body>
-        <form method="POST" enctype="multipart/form-data">
-            <fieldset>
-                <legend>Cadastro de Livros</legend>
+<body>
+    <div class="container">
+        <div class="container-escrita">
+            <h1 class="livro">Criar Livros</h1>
+            <form method="POST" enctype="multipart/form-data">
+                <div class="container-cores">
+                    <div class="container-capa">
+                        <img class="azul" src="../assets/images/azul.png">
+                        <img class="verde" src="../assets/images/verde.png">
+                        <img class="rosa" src="../assets/images/rosa.png">
+                        <img class="laranja" src="../assets/images/laranja.png">
+                        <img class="roxo" src="../assets/images/roxo.png">
+                    </div>
 
-                Nome: <input type="text" name="nome" value="<?php echo   $nomeLivro ?>">
-                <span class="obrigatorio">*<?php echo   $nomeLivroErro ?></span>
-                <br>
-                <input type="file" name="image">
-                <br>
-                <input type="submit" value="Salvar" name="submit">
-            </fieldset>
-        </form>
-        <span><?php echo $msgErro ?></span>
-        <h3><a href="../inicial.php">Principal</a></h3>
-    </body>
+                    <div class="container-criar">
+                        <div class="juncao-botao1">
+                            <label for="upload_image" class="capa">
+                                <img class="botao" src="../assets/images/botao.png">
+                            </label>
+                            <input type="file" id="upload_image" name="image">
+                        </div>
+                    </div>
+                    <div class="container-adicionar">
+                        <h3 class="adicionar">Adicionando capa</h3>
+                    </div>
+                </div>
+                <h2 class="titulo-criar_livros">Título</h2>
+                <img src="" alt="" id="imagemTemp">
+                <div class="botao-criar_livros">
+                    <input type="text" name="nome" value="Adicione seu título">
+                    <span class="obrigatorio"> * <?php echo   $nomeLivroErro ?></span>
 
-    </html>
+                    <div class="botao-flex">
+                        <input class="botao-salvar"  type="submit" value="Salvar" name="submit">
+                    </div>
+                </div>
+            </form>
+            <script>
+                const imgInp = document.getElementById("upload_image");
+                const imagemTemp = document.getElementById("imagemTemp");
+
+                imgInp.onchange = evt => {
+                    const [file] = imgInp.files
+                    if (file) {
+                        imagemTemp.src = URL.createObjectURL(file)
+                    }
+                }
+            </script>
+            <span class="erro"><?php echo $msgErro ?></span>
+</body>
+
+</html>
--- paginas/list_livro.php
+++ paginas/list_livro.php
@@ -3,12 +3,12 @@ include "../include/MySql.php";
 session_start();
 $nomeLivro =
 
-$sql = $pdo->prepare('SELECT * FROM livros WHERE idEmail=?');
-if ($sql->execute(array( $_SESSION['idEmail']))) {
+  $sql = $pdo->prepare('SELECT * FROM livros WHERE idEmail=?');
+if ($sql->execute(array($_SESSION['idEmail']))) {
   $info = $sql->fetchAll(PDO::FETCH_ASSOC);
   if (count($info) > 0) {
     foreach ($info as $key => $values) {
-      $_SESSION['nomeLivro'] =$nomeLivro;
+      $_SESSION['nomeLivro'] = $nomeLivro;
       $nomeLivro = "";
     }
     //header('location:paginas/list_usuario.php');
@@ -17,6 +17,7 @@ if ($sql->execute(array( $_SESSION['idEmail']))) {
   echo "<tr>";
   echo "  <th>Nome do Livro</th>";
   echo "  <th>Usuário</th>";
+  echo "  <th>Imagem</th>";
   echo "  <th>Selecionar</th>";
   echo "  <th>Excluir</th>";
   echo "</tr>";
@@ -24,6 +25,7 @@ if ($sql->execute(array( $_SESSION['idEmail']))) {
     echo "<tr>";
     echo "<td>" . $value['nomeLivro'] . "</td>";
     echo "<td>" . $value['idEmail'] . "</td>";
+    echo '<td><img src="data:image/png;base64,' . base64_encode($value['capaLivro']) . '" />1</td>';
     echo "<td><center><a href='../inicial.php?id=" . $value['nomeLivro'] . "'>(+)</a></center></td>";
     echo "<td><center><a href='del_livro.php?id=" . $value['nomeLivro'] . "'>(-)</a></center></td>";
     echo "<td><center><a href='alt_livro.php?id=" . $value['nomeLivro'] . "'>(😥)</a></center></td>";
@@ -31,4 +33,3 @@ if ($sql->execute(array( $_SESSION['idEmail']))) {
   }
   echo "</table>";
 }
-?>
