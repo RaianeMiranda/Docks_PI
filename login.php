b48b0c3d04d0b9a4455904eb0ef219a861aa37df
Raiane Miranda <raiane.oliveira@alunos.sc.senac.br>
2022-09-26 09:26:54 -0300

    alterando front do login

--- login.php
+++ login.php
@@ -44,67 +44,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-    <div class="login-form">
-        <div class="todoLogin">
-            <div class="login_cima">
-                <div class="container">
-                    <div class="flex2">
-                        <img class="logo" src="assets/images/logo.png" alt="logo do site, sendo um círculo com nosso mascote docks, um pato dentro desse círculo.">
-                    </div>
-                    <div class="flex3">
-                        <span class="close">&times;</span>
-                    </div>
-                </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/login1.css">
+</head>
 
+<body>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+        <div class="container_inicial2">
+            <div class="image_top">
+                <img class="logo-img" src="assets/images/docks.png" alt="">
+            </div>
+            <div class="bem-vindo">
                 <h1 class="slogan">Bem vindo(a) ao Docks</h1>
                 <p class="slogan">Aqui é o lugar para suas histórias</p>
             </div>
-
-            <div class="login-meio">
-                <fieldset>
-                    <legend for="idEmail">Digite seu E-mail</legend>
-                    <input class="norm-login" type="text" name="idEmail" id="idEmail" placeholder="E-mail">
-                </fieldset>
-
-                <fieldset>
-                    <legend for="Senha">Digite sua Senha</legend>
-                    <input class="norm-login" type="password" name="senha" id="Senha" placeholder="Senha">
-                </fieldset>
-
-                <input type="submit" name="continuar" id="continuar" value="Continuar" class="continuar"></input>
-
-                <p class="ou">OU</p>
+            <fieldset class="form-fieldset">
+                <legend for="idEmail">Digite seu E-mail</legend>
+                <input type="text" id="idEmail" name="idEmail" placeholder="E-mail">
+            </fieldset>
+            <fieldset class="form-fieldset">
+                <legend for="Senha">Digite sua Senha</legend>
+                <input type="password" id="Senha" name="senha" placeholder="Insira sua senha">
+            </fieldset>
+            <div class="btn-continuar">
+                <input type="submit" name="continuar" id="continuar" value="Continuar" class="continuar">
             </div>
-
-            <div class="login-baixo">
-
-                <fieldset>
-                    <div class="flex">
-                        <img class="icon-login" src="assets/images/facebook.png" alt="Faça seu login com o Facebook">
-                        <a href="#"><button class="login-with">Continue com o Facebook </button></a>
-                    </div>
-                </fieldset>
-
-                <fieldset>
-                    <div class="flex">
-
-                        <img class="icon-login" src="assets/images/google.png" alt="Faça seu login com o Facebook">
-                        <a href="#"><button class="login-with">Continue com o Google </button></a>
-                    </div>
-                </fieldset>
-                <p>Ainda não é um membro?</p>
-                <p><button class="membro"><a href="paginas/cad_usuario.php">Cadastre-se</a></button></p>
-
-            </div>
+            <p class="ou">OU</p>
+            <fieldset>
+                <div class="flex">
+                    <img class="icon-login" src="assets/images/facebook.png" alt="Faça seu login com o Facebook">
+                    <a href="#"><button class="login-with">Continuar com o Facebook </button></a>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div class="flex">
+                    <img class="icon-login" src="assets/images/google.png" alt="Faça seu login com o Facebook">
+                    <a href="#"><button class="login-with">Continuar com o Google </button></a>
+                </div>
+            </fieldset>
+            <p class="membro2">Ainda não é um membro?<a href="paginas/cad_usuario.php" class="membro-cadastro2"> Criar uma conta</a></p>
         </div>
-    </div>
-</form>
-<span><?php echo $msgErro ?></span>
-<!-- 
-            <div class="login-meio">
-            <label for="e-mail">E-mail</label>
-            <input class="" type="text" name="idEmail" id="e-mail" placeholder="Digite seu E-mail">
-            </div>
-        -->
+</body>
+
+</html>
